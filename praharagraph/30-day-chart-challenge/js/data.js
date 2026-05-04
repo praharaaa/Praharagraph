e9d5ac0a89a16e0118bd25c6fa89284364ba23d3
praharaaa <39756845+praharaaa@users.noreply.github.com>
2026-05-04 17:42:45 +0000

    update 30DCC layout + data.js

---
+++ praharagraph/30-day-chart-challenge/js/data.js
@@ -0,0 +1,207 @@
+const weeks = [
+  {
+    title: "Week 1: Comparisons",
+    days: [
+      {
+        day: 1,
+        prompt: "Part-to-Whole",
+        embed: `<iframe title="How Many Public Holidays Does Each Southeast Asian Countries Get Per Month in 2026?" aria-label="Small multiple column chart" id="datawrapper-chart-Ux1bW" src="https://datawrapper.dwcdn.net/Ux1bW/3/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="615" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 2,
+        prompt: "Pictogram",
+        embed: `<iframe title="When It Comes to Palm Oil, Indonesia Alone Accounts for Nearly 60% of the World's Total Production." aria-label="Stacked column chart" id="datawrapper-chart-RNaM0" src="https://datawrapper.dwcdn.net/RNaM0/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="862" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 3,
+        prompt: "Mosaic",
+        embed: `<iframe title="People Still Prefer Neutral Car Colors" aria-label="Stacked Bars" id="datawrapper-chart-42LOf" src="https://datawrapper.dwcdn.net/42LOf/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="357" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 4,
+        prompt: "Slope",
+        embed: `<iframe title="Nuclear Electricity Alone Shows No Sign of Cost Reduction Throughout its Development" aria-label="Small multiple line chart" id="datawrapper-chart-pOhNU" src="https://datawrapper.dwcdn.net/pOhNU/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="615" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 5,
+        prompt: "Experimental",
+        embed: `<iframe title="Table of Nuclides" aria-label="Scatter Plot" id="datawrapper-chart-xHJQv" src="https://datawrapper.dwcdn.net/xHJQv/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="719" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 6,
+        prompt: "Reporters Without Borders Data Day",
+        embed: `<iframe title="Indonesia's Press Freedom Ranking in Southeast Asia Has Been Declining Since 2015" aria-label="Line chart" id="datawrapper-chart-nbUTR" src="https://datawrapper.dwcdn.net/nbUTR/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="649" data-external="1"></iframe>`,
+        commentary: ""
+      }
+    ]
+  },
+  {
+    title: "Week 2: Distributions",
+    days: [
+      {
+        day: 7,
+        prompt: "Multiscale",
+        embed: `<iframe title="Indonesia's 2025 Tax Revenue Target Was Higher Than What the Government Collected" aria-label="Bullet Bars" id="datawrapper-chart-x06UR" src="https://datawrapper.dwcdn.net/x06UR/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="486" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 8,
+        prompt: "Circular",
+        embed: `<iframe title="Mineral Products and Metals Lead Indonesia's Exports Product in 2024" aria-label="Donut Chart" id="datawrapper-chart-M8CXf" src="https://datawrapper.dwcdn.net/M8CXf/2/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="498" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 9,
+        prompt: "Wealth",
+        embed: `<iframe title="How Much Did Indonesia's DPR Members Declare in 2025?" aria-label="Choropleth map" id="datawrapper-chart-HiB0L" src="https://datawrapper.dwcdn.net/HiB0L/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="1501" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 10,
+        prompt: "Pop Culture",
+        embed: `<iframe title="Grammy Debut Hits: First Single, First Win" aria-label="Scatter Plot" id="datawrapper-chart-e4ac2" src="https://datawrapper.dwcdn.net/e4ac2/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="906" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 11,
+        prompt: "Physical",
+        embed: `<iframe title="How Indonesian Ultrarunners Accumulated Their Kilometers in 2025" aria-label="Scatter Plot" id="datawrapper-chart-mSEwz" src="https://datawrapper.dwcdn.net/mSEwz/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="842" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 12,
+        prompt: "FlowingData Theme Day",
+        embed: `<iframe title="Changing ranks for fields of study in the United States" aria-label="Line chart" id="datawrapper-chart-Gcx78" src="https://datawrapper.dwcdn.net/Gcx78/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="1157" data-external="1"></iframe>`,
+        commentary: ""
+      }
+    ]
+  },
+  {
+    title: "Week 3: Relationships",
+    days: [
+      {
+        day: 13,
+        prompt: "Ecosystems",
+        embed: `<iframe title="The Impact of the PPCDAm on Deforestation in The Legal Amazon of Brazil" aria-label="Stacked column chart" id="datawrapper-chart-DpHRC" src="https://datawrapper.dwcdn.net/DpHRC/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="877" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 14,
+        prompt: "Trade",
+        embed: `<iframe title="How One Political Move Pushed Oil Prices From $65 to $114" aria-label="Line chart" id="datawrapper-chart-rb33G" src="https://datawrapper.dwcdn.net/rb33G/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="706" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 15,
+        prompt: "Correlation",
+        embed: `<iframe title="There Is No Correlation Between the Sepal Length of Iris Setosa Varieties and the Bill Length of Gentoo Penguins on Biscoe Island." aria-label="Scatter Plot" id="datawrapper-chart-a4LYN" src="https://datawrapper.dwcdn.net/a4LYN/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="707" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 16,
+        prompt: "Causation",
+        embed: `<iframe title="How the 2024 solar eclipse path shaped search interest in the United States" aria-label="Choropleth map" id="datawrapper-chart-fFBl1" src="https://datawrapper.dwcdn.net/fFBl1/2/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="573" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 17,
+        prompt: "Remake",
+        embed: `<iframe title="Life Expectancy Among Native White Males in the U.S., 1860" aria-label="Bullet Bars" id="datawrapper-chart-QhIHX" src="https://datawrapper.dwcdn.net/QhIHX/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="2130" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 18,
+        prompt: "UNICEF Data Day",
+        embed: `<iframe title="High Literacy Rates Among Young Women Don't Always Mean They Get to Finish High School" aria-label="Scatter Plot" id="datawrapper-chart-yMHux" src="https://datawrapper.dwcdn.net/yMHux/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="837" data-external="1"></iframe>`,
+        commentary: ""
+      }
+    ]
+  },
+  {
+    title: "Week 4: Timeseries",
+    days: [
+      {
+        day: 19,
+        prompt: "Evolution",
+        embed: `<iframe title="Flowering Plants Conquered Most of Earth's Plant Species in Under 30 Million Years" aria-label="Area Chart" id="datawrapper-chart-8sauJ" src="https://datawrapper.dwcdn.net/8sauJ/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="646" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 20,
+        prompt: "Global Change",
+        embed: `<iframe title="Post-Pandemic Life Evaluation Ratings: Some Regions Thrived, Others Didn't" aria-label="Choropleth map" id="datawrapper-chart-f0fiR" src="https://datawrapper.dwcdn.net/f0fiR/2/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="490" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 21,
+        prompt: "Historical",
+        embed: `<iframe title="20 Years of &quot;Idul Fitri&quot; on Google Trends Indonesia: How Searches Move Every Year" aria-label="Small multiple line chart" id="datawrapper-chart-Zckwu" src="https://datawrapper.dwcdn.net/Zckwu/3/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="864" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 22,
+        prompt: "New Tool",
+        embed: `<iframe title="AI as the New Work Tool. But Where's the Work Itself?" aria-label="Line chart" id="datawrapper-chart-uJGsy" src="https://datawrapper.dwcdn.net/uJGsy/2/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="591" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 23,
+        prompt: "Seasons",
+        embed: `<iframe title="London Brought a Drizzle while Jakarta Brought a Flood." aria-label="Line chart" id="datawrapper-chart-1mlwA" src="https://datawrapper.dwcdn.net/1mlwA/4/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="615" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 24,
+        prompt: "South China Morning Post Theme Day",
+        embed: `<iframe title="Iraq's Bloody Toll" aria-label="Line chart" id="datawrapper-chart-6YNC1" src="https://datawrapper.dwcdn.net/6YNC1/3/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="841" data-external="1"></iframe>`,
+        commentary: ""
+      }
+    ]
+  },
+  {
+    title: "Week 5: Uncertainties",
+    days: [
+      {
+        day: 25,
+        prompt: "Space",
+        embed: `<iframe title="What if You Could Draw the Entire History of the Universe on a Single Diagram?" aria-label="Scatter Plot" id="datawrapper-chart-xd3Un" src="https://datawrapper.dwcdn.net/xd3Un/2/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="787" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 26,
+        prompt: "Trend",
+        embed: `<iframe title="300 Months of Shaking Ground" aria-label="Scatter Plot" id="datawrapper-chart-puKTe" src="https://datawrapper.dwcdn.net/puKTe/2/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="810" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 27,
+        prompt: "Animation",
+        embed: `<iframe title="The Top 50 Animated Films Over Time" aria-label="Scatter Plot" id="datawrapper-chart-Aj8tX" src="https://datawrapper.dwcdn.net/Aj8tX/2/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="665" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 28,
+        prompt: "Modeling",
+        embed: `<iframe title="When a Scatter Plot Becomes a Drawing" aria-label="Scatter Plot" id="datawrapper-chart-bDvpM" src="https://datawrapper.dwcdn.net/bDvpM/2/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="635" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 29,
+        prompt: "Monochrome",
+        embed: `<iframe title="Mona Lisa" aria-label="Scatter Plot" id="datawrapper-chart-fJG9c" src="https://datawrapper.dwcdn.net/fJG9c/3/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="1011" data-external="1"></iframe>`,
+        commentary: ""
+      },
+      {
+        day: 30,
+        prompt: "Global Health Data Exchange Data Day",
+        embed: `<iframe title="Household TB Exposure Is Highest in India" aria-label="Bar Chart" id="datawrapper-chart-SpZ0P" src="https://datawrapper.dwcdn.net/SpZ0P/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="607" data-external="1"></iframe>`,
+        commentary: ""
+      }
+    ]
+  }
+];
